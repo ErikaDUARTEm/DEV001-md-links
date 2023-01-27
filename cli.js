2015b42bedd9c64f7ce4355eac7122076a5312e0
Erika Duarte <erika.duarte.mora@gmail.com>
2023-01-26 22:22:19 -0500

    Funciones de mensajes y modifique unos test, además añadi carpeta sin md

--- cli.js
+++ cli.js
@@ -2,20 +2,20 @@
 const { mdLinks } = require('./index.js');
 const  colors = require('colors');
 const emoji = require('node-emoji');
-const { Welcome, Error } = require('./message.js');
+const { Welcome, Error, Help } = require('./message.js');
 const { totalLinks, uniqueLinks, brokenLinks } = require('./cliFunct.js');
-
 
 const options = process.argv.slice(2);
 const userPath = process.argv[2];
 const validate = options.includes('--validate') || options.includes('--v');
 const stats = options.includes('--stats') || options.includes('--s');
+const help = options.includes('--help') || options.includes('--h');
 
 if(options.length === 1) {
     mdLinks(userPath, { validate:false } )
     .then((promise)=>{
         Welcome();
-        console.log(promise.flat())
+        console.log(promise.flat());
     })
     .catch((error) => {
         Error(error);
@@ -25,9 +25,9 @@ if(options.length === 1) {
     mdLinks(userPath, { validate: true})
     .then((promise)=>{
             Welcome();
-            console.log(totalLinks(promise) .cyan)
-            console.log(uniqueLinks(promise) .magenta)
-            console.log(brokenLinks(promise) .red)
+            console.log(`💜💜💜`, totalLinks(promise) .cyan.bold, `💜💜💜`);
+            console.log(`🔗🔗🔗`, uniqueLinks(promise) .magenta.bold, `🔗🔗🔗`);
+            console.log(`❌❌❌`, brokenLinks(promise) .red.bold , `❌❌❌`);
     })
     .catch((error) => {
         Error(error);
@@ -36,7 +36,7 @@ if(options.length === 1) {
         mdLinks(userPath, {validate:true})
         .then((resp) => {
             Welcome();
-            console.log(resp.flat())  
+            console.log(resp.flat());  
         }).catch((error)=>{
             Error(error);
         })
@@ -44,11 +44,19 @@ if(options.length === 1) {
         mdLinks(userPath, {validate:true})
         .then((resp) => {
             Welcome();
-            console.log(totalLinks(resp) .cyan)
-            console.log(uniqueLinks(resp) .magenta)
+            console.log(`💜💜💜`, totalLinks(resp) .cyan.bold,`💜💜💜`);
+            console.log(`🔗🔗🔗`, uniqueLinks(resp) .magenta.bold ,`🔗🔗🔗`);
         }).catch((error)=> { 
             Error(error);
         })
-    };
+    }else if(help){
+        mdLinks(userPath, { validate:true })
+        .then(Help())
+        .catch((error)=> Error(error));
+    }else {
+        mdLinks(userPath, { validate:true })
+        .then(Error('✨✨✨✨  Please enter a file or directory path to continue ✨ ✨✨✨✨'))
+        .catch((error)=>console.log(Error(error)));
+    }
 }
 
